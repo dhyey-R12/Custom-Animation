8ec1a7f5ce6f4931e7fb39bef55b9944677e9549
dhyey-R12 <rajparadhyey12@gmail.com>
2024-06-19 23:59:25 +0530

    Dynamic Image Get

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,45 @@
-// let sliderWidth = document.querySelector(".slider");
-// document.body.style.height = `${sliderWidth.scrollHeight}px`;
-// console.log("body", document.body.style.height);
-// console.log("sliderWidth",sliderWidth.scrollHeight);
+document.addEventListener("DOMContentLoaded", (event) => {
+    // gsap.registerPlugin(ScrollTrigger)
+    // gsap code here!
+    console.log("test");
 
-// const handleScroll = () => {
-//     const scrollPos = window.scrollY;
-//     const slider = document.querySelector(".slider");
-//     const initialTransform = `translate3d(-50%, -50%, 0) rotateX(0deg) rotateY(-25deg) rotateZ(-120deg)`;
-//     const zOffset = scrollPos * 0.5;
-//     slider.style.transform = `${initialTransform} translateY(${zOffset}px)`;
-// }
-// window.addEventListener("scroll", handleScroll);
-// let element = document.getElementsByClassName("card");
-// for (let i = 0; i < element.length; i++) {
-//     element[i].addEventListener("mouseover",  function () {
-//         this.style.left = "15%";
-//     });
+    createSlide(80);
 
-//     element[i].addEventListener("mouseout",function() {
-//         this.style.left = "0%";
-//     });
-// }
+    let sliderWidth = document.getElementById('slider');
+    document.body.style.height = `${sliderWidth.scrollHeight}px`;
+});
+
+function createSlide(sliderNumber) {
+    const slider = document.getElementById('slider');
+
+    for (let i = 0; i < sliderNumber; i++) {
+        const newSlide = document.createElement('div');
+        const newSlideImage = document.createElement('img');
+        newSlideImage.setAttribute("src", `images/image_${randomIntFromInterval(1, 4)}.jpg`);
+        newSlide.setAttribute("class", "card");
+        newSlide.appendChild(newSlideImage);
+        newSlide.addEventListener("mouseover", function () {
+            this.style.left = "15%";
+        });
+        newSlide.addEventListener("mouseout", function () {
+            this.style.left = "0%";
+        });
+        slider.appendChild(newSlide);
+    }
+
+}
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+const handleScroll = () => {
+    const scrollPos = window.scrollY;
+    const slider = document.querySelector(".slider");
+    const initialTransform = `translate3d(-50%, -50%, 0) rotateX(0deg) rotateY(-25deg) rotateZ(-120deg)`;
+    const zOffset = scrollPos * 0.5;
+    slider.style.transform = `${initialTransform} translateY(${zOffset}px)`;
+}
+
+window.addEventListener("scroll", handleScroll);
