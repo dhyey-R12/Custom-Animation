808606153007e3f9393538dbcba5e044ca12c132
dhyey-R12 <rajparadhyey12@gmail.com>
2024-06-28 00:18:11 +0530

    code improve

--- js/custom.js
+++ js/custom.js
@@ -6,10 +6,17 @@ document.addEventListener("DOMContentLoaded", async (event) => {
         const imageList = await getImages();
         isLoading(false);
         createSlide(imageList);
-        let sliderWidth = document.getElementById('slider');
-        document.body.style.height = `${sliderWidth.scrollHeight}px`;
+        // let sliderWidth = document.getElementById('slider');
+        // document.body.style.height = `${sliderWidth.scrollHeight}px`;
     } catch (error) {
-        console.error("Error while getting image from API : ", error);
+        isLoading(false);
+        if (error && error.message) {
+            const slider = document.getElementById('slider');
+            const responseEl = document.createElement('div');
+            responseEl.setAttribute("class", "card error_card");
+            slider.appendChild(responseEl);
+            responseEl.innerHTML = `<p>${error.message}</p>`;
+        }
     }
 });
 
@@ -19,8 +26,12 @@ function getImages() {
 
         xhr.open("GET", BASE_URL, true);
         xhr.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                resolve(JSON.parse(this.responseText));
+            if (this.readyState === 4) {
+                if (this.status === 200) {
+                    resolve(JSON.parse(this.responseText));
+                } else {
+                    reject({ message: "Error while getting images!" });
+                }
             }
         }
         xhr.send();
@@ -30,7 +41,7 @@ function getImages() {
 function createSlide(imageList) {
     const slider = document.getElementById('slider');
 
-    for (let i = 0; i < (imageList.length / 20); i++) {
+    for (let i = 0; i < 11; i++) {
         const newSlide = document.createElement('div');
         const newSlideImage = document.createElement('img');
         newSlideImage.setAttribute("src", `${imageList[i].url}`);
@@ -44,18 +55,22 @@ function createSlide(imageList) {
         });
         slider.appendChild(newSlide);
     }
+
+    // horizontalScroll();
+    checkWidth();
 }
 
 
 const handleScroll = () => {
     const scrollPos = window.scrollY;
     const slider = document.querySelector(".slider");
-    const initialTransform = `translate3d(-50%, -50%, 0) rotateX(0deg) rotateY(-25deg) rotateZ(-120deg)`;
+    const initialTransform = ``;
+    // const initialTransform = `translate3d(-50%, -50%, 0) rotateX(0deg) rotateY(-25deg) rotateZ(-120deg)`;
     const zOffset = scrollPos * 0.5;
     slider.style.transform = `${initialTransform} translateY(${zOffset}px)`;
 }
 
-window.addEventListener("scroll", handleScroll);
+// window.addEventListener("scroll", handleScroll);
 
 
 
@@ -131,5 +146,89 @@ function frame() {
             });
             document.getElementById("percent").innerHTML = "( " + width + "%" + " )";
         }
+    }
+}
+
+
+// horizontal scroll JS
+gsap.registerPlugin(ScrollTrigger);
+
+// let horizontalSection = document.querySelector('.slider_section .slider');
+
+// console.log(horizontalSection.scrollWidth);
+
+// gsap.to(horizontalSection, {
+//   x: () => horizontalSection.scrollWidth * -1,
+//   xPercent: 100,
+//   scrollTrigger: {
+//     trigger: horizontalSection,
+//     start: 'top top',
+//     end: 'bottom bottom',
+//     pin: '.slider_section ',
+//     scrub: true,
+//     invalidateOnRefresh: true
+//   }
+// });
+
+
+// function horizontalScroll() {
+
+//     let sections = gsap.utils.toArray(".card");
+
+//     gsap.to(sections, {
+//         xPercent: -100 * (sections.length - 1),
+//         ease: "none",
+//         scrollTrigger: {
+//             trigger: ".slider_section .slider",
+//             pin: true,
+//             scrub: 1,
+//             // snap: 1 / (sections.length - 1),
+//             end: () => "+=" + document.querySelector(".slider_section .slider").scrollWidth,
+//         }
+//     });
+// }
+
+// let countS = 0;
+// jQuery(".slider_section .slider > *").each(function () {
+//     countS += jQuery(this).outerWidth();
+//     console.log(jQuery(this).outerWidth());
+// })
+// console.log(countS);
+let countS = 0;
+jQuery(".slider_section .slider > *").each(function () {
+    countS += jQuery(this).outerWidth();
+    console.log(jQuery(this).outerWidth());
+})
+console.log(countS);
+
+function checkWidth() {
+    var windowSize = $(window).width();
+
+    if (windowSize > 768) {
+        const process = document.querySelector('.slider_section .slider')
+        if ((typeof (process) != 'undefined' && process != null)) {
+            let sections = gsap.utils.toArray('.slider_section .slider > *');
+            gsap.to(process, 3, {
+                x: () => (process.scrollWidth - window.outerWidth + 100) * -1,
+                // xPercent: -100,
+                // ease: "",
+                ease: "power1.ease",
+                duration: 0.5,
+                scrollTrigger: {
+                    markers: false,
+                    trigger: process,
+                    pin: '.slider_section',
+                    start: "top 8%",
+                    // end: '2000px',
+                    end: sections,
+                    // end: "bottom top",
+                    scrub: true,
+                    invalidateOnRefresh: true,
+                },
+            });
+        }
+        console.log("window.innerWidth", window.innerWidth);
+        console.log("process.scrollWidth", process.scrollWidth);
+        console.log("window.outerWidth - process.scrollWidth", window.outerWidth - process.scrollWidth);
     }
 }
